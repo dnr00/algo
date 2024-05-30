4a2aac3748759531b9f193d64149dcaeeadd5c4b
uksang 29 <dnr00@naver.com>
2024-05-30 05:46:13 +0000

    calculator

--- playground/projects/playground/smart_contracts/hello_world/calculator.py
+++ playground/projects/playground/smart_contracts/hello_world/calculator.py
@@ -1,9 +1,13 @@
 import beaker as bk
 import pyteal as pt
 
-app = bk.Application("Calculator")
+class MyState:
+    result = bk.GlobalStateValue(pt.TealType.uint64)    
+
+
+app = bk.Application("Calculator", state=MyState())
 
 @app.external
 def add(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
     add_result = a.get() + b.get()
-    return output.set(add_result)
+    return pt.Seq()
